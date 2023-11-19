91259a1f6f2cbe231038e481c60d7e832fc2f0fc
Lana Dzyuban <svitlana.dzyuban@gmail.com>
2023-11-19 11:55:20 -0500

    clean canvas and restart the game

--- index.js
+++ index.js
@@ -5,51 +5,8 @@ window.onload = () => {
   const step = 25;
   let history = [];
 
-  // Draw the axes
-  ctx.beginPath();
-  ctx.moveTo(0, canvas.height / 2);
-  ctx.lineTo(canvas.width, canvas.height / 2);
-  ctx.moveTo(canvas.width / 2, 0);
-  ctx.lineTo(canvas.width / 2, canvas.height);
-  ctx.stroke();
-
-  // Draw the markers on X-axis
-  for (let i = step; i < canvas.width; i += step) {
-    ctx.beginPath();
-    ctx.moveTo(i, canvas.height / 2 - 5);
-    ctx.lineTo(i, canvas.height / 2 + 5);
-    ctx.stroke();
-
-     // Draw the label
-     let label = (i - canvas.width / 2) / step;
-    ctx.fillText(label, i - 5, canvas.height / 2 - 10);
-  }
-
-  // Draw the markers on Y-axis
-  for (let i = step; i < canvas.height; i += step) {
-    ctx.beginPath();
-    ctx.moveTo(canvas.width / 2 - 5, i);
-    ctx.lineTo(canvas.width / 2 + 5, i);
-    ctx.stroke();
-
-    // Draw the label
-    let label = (canvas.height / 2 - i) / step;
-    ctx.fillText(label, canvas.width / 2 + 10, i + 5);
-  }
-
-  // Draw arrow on axis-X
-  ctx.beginPath();
-  ctx.moveTo(canvas.width - 10, canvas.height / 2 - 5);
-  ctx.lineTo(canvas.width, canvas.height / 2);
-  ctx.lineTo(canvas.width - 10, canvas.height / 2 + 5);
-  ctx.stroke();
-
-  // Draw arrow on axis-Y
-  ctx.beginPath();
-  ctx.moveTo(canvas.width / 2 - 5, 10);
-  ctx.lineTo(canvas.width / 2, 0);
-  ctx.lineTo(canvas.width / 2 + 5, 10);
-  ctx.stroke();
+  // Draw the coordinate system
+  drawCoordinateSystem();
 
   ({ x: generatedX, y: generatedY } = generateRandomPoint());
   console.log("generated point: ", generatedX, generatedY);
@@ -109,6 +66,18 @@ window.onload = () => {
     renderHistory();
   });
 
+  document.getElementById('clean').addEventListener('click', function(e) {
+    e.preventDefault();
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    drawCoordinateSystem();
+    ({ x: generatedX, y: generatedY } = generateRandomPoint());
+    history = [];
+    document.getElementById('result').innerHTML = '';
+
+    redBall.classList.add('hidden');
+    renderHistory();
+  });
+
   // render history
   function renderHistory() {
     const historyElement = document.getElementById('history');
@@ -164,5 +133,55 @@ window.onload = () => {
     ctx.fill();
     ctx.stroke();
     return { x: x, y: y };
+  }
+
+  function drawCoordinateSystem() {
+    // Draw the axes
+    ctx.beginPath();
+    ctx.strokeStyle = 'black';
+    ctx.fillStyle = 'black';
+    ctx.moveTo(0, canvas.height / 2);
+    ctx.lineTo(canvas.width, canvas.height / 2);
+    ctx.moveTo(canvas.width / 2, 0);
+    ctx.lineTo(canvas.width / 2, canvas.height);
+    ctx.stroke();
+
+    // Draw the markers on X-axis
+    for (let i = step; i < canvas.width; i += step) {
+      ctx.beginPath();
+      ctx.moveTo(i, canvas.height / 2 - 5);
+      ctx.lineTo(i, canvas.height / 2 + 5);
+      ctx.stroke();
+
+      // Draw the label
+      let label = (i - canvas.width / 2) / step;
+      ctx.fillText(label, i - 5, canvas.height / 2 - 10);
+    }
+
+    // Draw the markers on Y-axis
+    for (let i = step; i < canvas.height; i += step) {
+      ctx.beginPath();
+      ctx.moveTo(canvas.width / 2 - 5, i);
+      ctx.lineTo(canvas.width / 2 + 5, i);
+      ctx.stroke();
+
+      // Draw the label
+      let label = (canvas.height / 2 - i) / step;
+      ctx.fillText(label, canvas.width / 2 + 10, i + 5);
+    }
+
+    // Draw arrow on axis-X
+    ctx.beginPath();
+    ctx.moveTo(canvas.width - 10, canvas.height / 2 - 5);
+    ctx.lineTo(canvas.width, canvas.height / 2);
+    ctx.lineTo(canvas.width - 10, canvas.height / 2 + 5);
+    ctx.stroke();
+
+    // Draw arrow on axis-Y
+    ctx.beginPath();
+    ctx.moveTo(canvas.width / 2 - 5, 10);
+    ctx.lineTo(canvas.width / 2, 0);
+    ctx.lineTo(canvas.width / 2 + 5, 10);
+    ctx.stroke();
   }
 }
